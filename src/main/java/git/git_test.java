05b9dcca8447eacabbd5afd4224dac7827d3ce30
cjq <cjq@cjq.com>
2021-11-11 11:30:02 +0800

    push commit

--- src/main/java/git/git_test.java
+++ src/main/java/git/git_test.java
@@ -7,5 +7,6 @@ public class git_test {
         System.out.println("hello git3!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
     }
 }
